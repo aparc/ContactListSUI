e21e08186cf12e738c1bb38f237d89fcf6408676
Andrei <a.parchukov@yandex.ru>
2022-09-24 14:26:11 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Андрей Парчуков on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactListSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactListSUI
+//
+//  Created by Андрей Парчуков on 24.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
